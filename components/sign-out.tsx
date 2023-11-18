1b6cb4668286f305e05d19bef58b9655a4526653
hongchiong <hongchiong@gmail.com>
2023-11-18 22:59:10 +0800

    adjust styles

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -4,7 +4,7 @@ import { signOut } from 'next-auth/react';
 export default function SignOut() {
   return (
     <button
-      className='text-stone-600 hover:text-stone-500 transition-all px-4 py-2 border m-2 rounded bg-slate-300'
+      className='text-stone-600 hover:text-stone-400 transition-all px-4 py-2 border m-2 rounded bg-slate-100'
       onClick={() => signOut()}>
       Logout!
     </button>
