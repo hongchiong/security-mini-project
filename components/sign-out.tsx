8df64532cdf96c8cf7dcce65b2ab2f7f602938c6
hongchiong <hongchiong@gmail.com>
2023-11-18 23:02:13 +0800

    Add redirect adter logout

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function SignOut() {
+  const router = useRouter();
   return (
     <button
       className='text-stone-600 hover:text-stone-400 transition-all px-4 py-2 border m-2 rounded bg-slate-100'
-      onClick={() => signOut()}>
+      onClick={() => {
+        signOut();
+        router.push('/');
+      }}>
       Logout!
     </button>
   );
