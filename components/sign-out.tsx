09cba0e54979daa1a60cec497ab2524b391be586
hongchiong <hongchiong@gmail.com>
2023-11-18 23:13:25 +0800

    Fix redirect

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -7,10 +7,7 @@ export default function SignOut() {
   return (
     <button
       className='text-stone-600 hover:text-stone-400 transition-all px-4 py-2 border m-2 rounded bg-slate-100'
-      onClick={() => {
-        signOut();
-        router.push('/');
-      }}>
+      onClick={() => signOut({ callbackUrl: '/' })}>
       Logout!
     </button>
   );
