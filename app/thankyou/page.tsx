2ad9cd438a0b6821b2f85b284a28669b963863d0
hongchiong <hongchiong@gmail.com>
2023-11-18 21:37:25 +0800

    Update style

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -4,10 +4,8 @@ import SignOut from '@/components/sign-out';
 
 export default function Home() {
   return (
-    <div className='flex h-screen bg-black'>
-      <div className='w-screen h-screen flex flex-col space-y-5 justify-center items-center'>
-        <SignOut />
-      </div>
+    <div className='flex h-screen w-screen items-center justify-center bg-gray-50'>
+      <SignOut />
     </div>
   );
 }
