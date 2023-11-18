06970e827c951101589dbdbf3474712fea589c86
hongchiong <hongchiong@gmail.com>
2023-11-18 21:07:21 +0800

    add sign out button

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -1,15 +1,5 @@
 'use client';
-// import { signOut } from 'next-auth/react';
-
-// export default function SignOut() {
-//   return (
-//     <button
-//       className='text-stone-400 hover:text-stone-200 transition-all'
-//       onClick={() => signOut()}>
-//       Logout!
-//     </button>
-//   );
-// }
+import { signOut } from 'next-auth/react';
 import DetailsForm from './detailsForm';
 
 export default function Login() {
@@ -21,6 +11,11 @@ export default function Login() {
         </div>
         <DetailsForm />
       </div>
+      <button
+        className='text-stone-400 hover:text-stone-200 transition-all'
+        onClick={() => signOut()}>
+        Logout!
+      </button>
     </div>
   );
 }
