f6876b0d12aade4346e1854e8fd0dd2babe22627
hongchiong <hongchiong@gmail.com>
2023-11-18 20:21:52 +0800

    Update prisma schema

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -1,11 +1,11 @@
-import { unstable_getServerSession } from "next-auth/next";
+import { unstable_getServerSession } from 'next-auth/next';
 
 export default async function AuthStatus() {
   const session = await unstable_getServerSession();
   return (
-    <div className="absolute top-5 w-full flex justify-center items-center">
+    <div className='absolute top-5 w-full flex justify-center items-center'>
       {session && (
-        <p className="text-stone-200 text-sm">
+        <p className='text-stone-400 text-sm'>
           Signed in as {session.user?.email}
         </p>
       )}
