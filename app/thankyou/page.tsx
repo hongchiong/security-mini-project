5ee171a293c9d2b04e71cec52e63cc37e9d267d7
hongchiong <hongchiong@gmail.com>
2023-11-18 22:47:17 +0800

    add styles

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -5,7 +5,12 @@ import SignOut from '@/components/sign-out';
 export default function Home() {
   return (
     <div className='flex h-screen w-screen items-center justify-center bg-gray-50'>
-      <SignOut />
+      <div className='z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl'>
+        <div className='flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16'>
+          <h3 className='text-xl font-semibold'>Thank you!</h3>
+        </div>
+        <SignOut />
+      </div>
     </div>
   );
 }
--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -4,7 +4,7 @@ import { signOut } from 'next-auth/react';
 export default function SignOut() {
   return (
     <button
-      className='text-stone-400 hover:text-stone-200 transition-all'
+      className='text-stone-400 hover:text-stone-200 transition-all px-4 py-2 border'
       onClick={() => signOut()}>
       Logout!
     </button>
